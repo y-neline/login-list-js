78e333f2b5d08190f27642c5cda2c649b0e6ac46
y-neline <89186278+y-neline@users.noreply.github.com>
2023-09-21 00:10:19 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    if(nameInput.value === '' || emailInput.value === ''){
+        // alert('Please enter fields');
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        // Clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+
+    }
+}
+
+
 // let score;
 // score = 10
 // console.log(score)
@@ -225,32 +256,3 @@
 //     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
 // })
 
-const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
-
-myForm.addEventListener('submit', onSubmit);
-
-function onSubmit(e) {
-    e.preventDefault();
-
-    if(nameInput.value === '' || emailInput.value === ''){
-        // alert('Please enter fields');
-        msg.classList.add('error');
-        msg.innerHTML = 'Please enter all fields';
-
-        setTimeout(() => msg.remove(), 3000);
-    } else {
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
-
-        userList.appendChild(li);
-
-        // Clear fields
-        nameInput.value = '';
-        emailInput.value = '';
-
-    }
-}
